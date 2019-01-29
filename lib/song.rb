bbc81bd62a1871afce3fece3ff584d018c94813b
Stephen Wright <scw248@gmail.com>
2019-01-29 20:37:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,9 +4,8 @@ class Song
   
   @@all = []
   
-  def initialize(name, artist, genre)
+  def initialize(name, genre)
     @name = name
-    @artist = artist
     @genre = genre
     @@all << self
   end
